937605e084b7dd0a4ce642f5195e73227e5ccdda
tensho1026 <tensho1026@icloud.com>
2025-02-04 23:54:39 +0900

    setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {};
+
+export const login = (req, res) => {
+  res.send("signup route");
+};
+
+export const logout = (req, res) => {
+  res.send("signup route");
+};
